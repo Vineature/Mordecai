7c1de88d799720f17ea000f215085a511811cba5
Vineature <Marcus.Lhotka@gmail.com>
2017-07-22 16:56:14 -0500

    Classes and Interfaces added

---
+++ Mordecai.Types/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mordecai.Types
+{
+    public class Character : IThing
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string SelfDescription { get; set; }
+        public string CharHistory { get; set; }
+        public string Race { get; set; }
+        public int SkillPoints { get; set; }
+        public int MagicPoints { get; set; }
+    }
+}
---
+++ Mordecai.Types/Place.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mordecai.Types
+{
+    public class Place : IThing
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int EncounterChance { get; set; }
+        public string StateChange { get; set; }
+
+
+    }
+}
