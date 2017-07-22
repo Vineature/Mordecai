5519d4e07ff297765329d3a126fa40437fe136c0
Vineature <Marcus.Lhotka@gmail.com>
2017-07-22 17:11:51 -0500

    Classes and interfaces added

--- Mordecai.Types/Weapon.cs
+++ Mordecai.Types/Weapon.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Mordecai.Types
 {
-    class Weapon
+    public class Weapon : IThing
     {
+        public int Id { get; set; }
+        public string Description { get; set; }
     }
 }
